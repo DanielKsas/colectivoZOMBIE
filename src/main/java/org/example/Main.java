bf925b619db3b17b38a1837df7547c600dc8ebd7
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-07-01 06:56:14 -0500

    feat se declararon variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,26 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Scanner keyEntry = new Scanner(System.in);
+        System.out.printf("***** RESIDENT EVIL CODE BY NO ONES *****");
 
+        //pERSONAJE LEON S KENNEDY
+        //nOMBRE
+        //eDAD
+        //cANTIDADvIDA
 
+        //pERSONAJE MR X (TITAN)
+        //nombre
+        //virus que lo infecto
+        //cantidad vida
+
+        final String personajeLeonSKennedy, nombre, edad;
+        final  String personajeMRX, name, virusqueloinfecto;
     }
 }
